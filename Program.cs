f03c259f576a6676e9f4aeb5e67db720d6923626
Josh Fryer <56700863+liljoshcodes@users.noreply.github.com>
2021-03-19 22:11:36 +0000

    Added insertion sort

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Insertion_sort_excersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int [10];
+
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                a[i] = rnd.Next(21);
+                Console.WriteLine("array index " + i + " = " + a[i]);
+            }
+            Console.WriteLine("\n");
+            InsertionSort(a);
+
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                Console.WriteLine("sorted array index " + i + " = " + a[i]);
+            }
+        }
+
+        private static void InsertionSort(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)//loop through array 
+            {
+                int k = i;
+                for (int j = i - 1; j >= 0; j--)// compare i to array left of it
+                {
+                    //swap if less than last number.
+                    if (a[k] < a[j])
+                    {
+                        //swap
+                        int lastNo = a[k];
+                        a[k] = a[j];
+                        a[j] = lastNo;
+                        k--;
+                    }
+                }
+            }
+        }
+    }
+}
